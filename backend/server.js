fb12bc23a804698fc6a9e1bd086565e2d009007c
Ankush Singh <rathorankushrana143@gmail.com>
2026-04-26 14:19:18 +0530

    Allow all origins in CORS for universal accessibility

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-rapidapi-key', 'x-rapidapi-host']
+}));
 app.use(express.json());
 
 // Routes
